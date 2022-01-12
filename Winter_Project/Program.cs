a22392900ec9b46314b4b9c0fb7ecc8a1f98c9de
Castor Dunkels <castordnk@gmail.com>
2022-01-12 13:26:53 +0100

    Start of game

---
+++ Winter_Project/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Raylib_cs;
+
+Raylib.InitWindow(800, 600, "Game");
+Raylib.SetTargetFPS(30);
+
+Rectangle playerRect = new Rectangle(30, 30, 50, 20);
+float speed = 2;
+
+while (Raylib.WindowShouldClose() == false) {
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_D)) playerRect.x += speed;
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_A)) playerRect.x -= speed;
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_W)) playerRect.y -= speed;
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_S)) playerRect.y += speed;
+
+    Raylib.BeginDrawing();
+
+    Raylib.ClearBackground(Color.SKYBLUE);
+    Raylib.DrawRectangleRec(playerRect, Color.GOLD);
+
+    Raylib.EndDrawing();
+}
