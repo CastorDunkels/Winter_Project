8c7ee246294a8b52c59a7c8470c2dae14a484ed7
Castor Dunkels <castordnk@gmail.com>
2022-02-01 14:31:14 +0100

    tiny change

--- Winter_Project/Program.cs
+++ Winter_Project/Program.cs
@@ -4,14 +4,18 @@ using Raylib_cs;
 Raylib.InitWindow(800, 600, "Game");
 Raylib.SetTargetFPS(30);
 
-Rectangle playerRect = new Rectangle(30, 30, 50, 20);
+float playerX = 10;
+float playerY = 580;
+
+Rectangle playerRect = new Rectangle(playerX, playerY, 50, 20);
 float speed = 2;
 
 while (Raylib.WindowShouldClose() == false) {
     if (Raylib.IsKeyDown(KeyboardKey.KEY_D)) playerRect.x += speed;
     if (Raylib.IsKeyDown(KeyboardKey.KEY_A)) playerRect.x -= speed;
-    if (Raylib.IsKeyDown(KeyboardKey.KEY_W)) playerRect.y -= speed;
     if (Raylib.IsKeyDown(KeyboardKey.KEY_S)) playerRect.y += speed;
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_W)) playerRect.y -= speed;
+
 
     Raylib.BeginDrawing();
 
