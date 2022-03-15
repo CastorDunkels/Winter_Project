a16ee8ab4b35fd39ebe036b5a607f0ce37848463
Castor Dunkels <castordnk@gmail.com>
2022-03-15 14:51:55 +0100

    trying to do something

--- Winter_Project/Program.cs
+++ Winter_Project/Program.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using Raylib_cs;
+﻿using Raylib_cs;
 
 Raylib.InitWindow(800, 600, "Game");
 Raylib.SetTargetFPS(30);
 
 const int GROUND = 580;
 float playerHealth = 3;
-//float playerHit = playerHealth--;
-float playerX = 400;
+float playerX = 100;
 float playerY = GROUND;
 float speed = 6;
 float WalkingSpeed = 0;
@@ -20,9 +18,13 @@ float leftWall = 0;
 float rightWall = 750;
 float enemyX = 500;
 float enemyY = GROUND - 20;
+float enemyHealth = 5;
+float weaponX = playerX;
+float weaponY = playerY;
 
 Rectangle playerRect = new Rectangle(playerX, playerY, 50, 20);
 Rectangle enemy = new Rectangle(enemyX, enemyY, 40, 40);
+Rectangle playerWeapon = new Rectangle(weaponX - 30, weaponY, 20, 2);
 
 while (Raylib.WindowShouldClose() == false)
 {
@@ -93,13 +95,9 @@ while (Raylib.WindowShouldClose() == false)
         playerX = rightWall;
     }
 
-    if (enemyX < playerX + 50 && enemyX > playerX - 50 && enemyY <= playerY + 25) //kollar om enemy colliderar med player för att se om hp ska minskas
+    if (enemyX < playerX + 45 && enemyX > playerX - 45 && enemyY <= playerY + 25) //kollar om enemy colliderar med player för att se om hp ska minskas
     {
         playerHealth--;
-    }
-    else
-    {
-        Console.WriteLine(playerHealth + 100);
     }
     if (playerHealth <= 0)
     {
@@ -107,7 +105,12 @@ while (Raylib.WindowShouldClose() == false)
         enemyX = enemyX + 1000000;
         Raylib.DrawText("YOU DIED", 250, 250, 60, Color.RED);
     }
-    Raylib.DrawText("Health: " + playerHealth, 40, 20, 30, Color.BLACK);  //skriver ut hur mycket hp man har
+    Raylib.DrawText("Health: " + playerHealth, 40, 20, 30, Color.RED);  //skriver ut hur mycket hp man har
+
+    if (enemyHealth <= 0)
+    {
+        
+    }
 
     if (playerY == GROUND)
     {
@@ -118,10 +121,17 @@ while (Raylib.WindowShouldClose() == false)
         Gravity += Acceleration;  //detta gör så att när man är över marken så går gravity igång 
     }
 
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+    {
+        Raylib.DrawRectangleRec(playerWeapon, Color.WHITE);
+    }
+
     playerRect.y = playerY;  //detta gör så att visuella delen och modellen är separata
     playerRect.x = playerX;
     enemy.x = enemyX;
     enemy.y = enemyY;
+    playerWeapon.y = weaponY;
+    playerWeapon.x = weaponX;
 
     Raylib.BeginDrawing();
 
